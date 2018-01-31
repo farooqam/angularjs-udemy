a800104b12636332d24c2c45a74c1c17989f5c99
Farooq Mahmud <fmahmud@lowes.com>
2018-01-31 09:29:12 -0800

    app1 - made controller minification safe

--- app1/app.js
+++ app1/app.js
@@ -1,12 +1,6 @@
 var app = angular.module('app1', ['ngResource']);
 
-app.controller('mainController', function($log, $resource) {
+app.controller('mainController', ['$log', '$resource', function($log, $resource) {
     this.name = '';
-
-    let things = [1,2,3, function(args) {
-        $log.info(args);
-    }];
-    
-    $log.info(things);
-    things[3]('hello function!');
-});
+    $log.log($resource);
+}]);
