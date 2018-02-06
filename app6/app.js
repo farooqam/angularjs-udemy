2233951c8673affccc61ffa39737cc7dfcedba45
farooqam <farooq_am@hotmail.com>
2018-02-06 13:12:26 -0800

    app6

--- app6/app.js
+++ app6/app.js
@@ -4,25 +4,41 @@ var app = angular.module('app1', []);
 
 app.controller('mainController', ['$log', '$filter', '$http', function($log, $filter, $http) {
     var self = this;
+    var handleLength = 5;
+    self.handle = '';
+
+    self.showWarning = function() {
+        return self.handle.length < handleLength;
+    }
     
+    self.showDanger = function() {
+        return self.handle.length > handleLength;
+    }
+
     self.onClick = function() {
         $log.info('Button clicked!');
+    };
+
+    self.toLowerCase = function(text) {
+        return $filter('lowercase')(text);
     };
 
     var rules = {};
     self.gotRules = false;
 
-    $http({
-        method : "GET",
-        url : "http://demo1621533.mockable.io/rules"
-    }).then(function mySuccess(response) {
-        $log.info(response.data);
-        rules = response.data;
-        self.gotRules = true;
-    }, function myError(response) {
-        $log.error(response.statusText);
-    });
-
+    self.getRules = function() {
+        $http({
+            method : "GET",
+            url : "http://demo1621533.mockable.io/rules"
+        }).then(function mySuccess(response) {
+            $log.info(response.data);
+            rules = response.data;
+            self.gotRules = true;
+        }, function myError(response) {
+            $log.error(response.statusText);
+        });
+    };
+    
     self.getRules = function() {
         return rules;
     };
