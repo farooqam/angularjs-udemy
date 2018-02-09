f245f8965d9cf645902415f409d0da5bb5b58448
Farooq Mahmud <fmahmud@lowes.com>
2018-02-09 10:49:03 -0800

    app10 - initial checkin - search component

---
+++ app10/app/search/search.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('myApp.search', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/search', {
+    templateUrl: 'search/search.html',
+    controller: 'SearchCtrl'
+  });
+}])
+
+.controller('SearchCtrl', ['$log', function($log) {
+  var self = this;
+  
+}]);
