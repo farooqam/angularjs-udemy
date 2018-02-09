4d60ba933fc6a2b6ae84447275f42f5fc88c88f8
Farooq Mahmud <fmahmud@lowes.com>
2018-02-09 15:53:24 -0800

    app10 - passing data to directives

--- app10/app/search/search.js
+++ app10/app/search/search.js
@@ -12,10 +12,25 @@ angular.module('myApp.search', ['ngRoute'])
 .controller('SearchCtrl', ['$log', function($log) {
   var self = this;
   
+  self.person = {
+      name: 'Bubba Gump',
+      address: '100 1st Ave, New York, NY 10010'
+  };
+
+  self.metadata = {
+      id: 100,
+      description: 'foo'
+  };
+
 }])
 .directive("searchResult", ['$log', function($log) {
     return {
         templateUrl: 'search/searchResult.html',
-        replace: true
+        replace: true,
+        scope: {
+            header: '@',
+            text: '@',    
+            metadata: '='        
+        }
     };
 }]);
