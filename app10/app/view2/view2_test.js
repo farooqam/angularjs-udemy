f179a02457637c24be3c28864cbe190a12653ceb
Farooq Mahmud <farooq_am@hotmail.com>
2018-02-10 00:40:06 -0800

    app10 - unit tests.

--- app10/app/view2/view2_test.js
+++ app10/app/view2/view2_test.js
@@ -4,6 +4,24 @@ describe('myApp.view2 module', function() {
 
   beforeEach(module('myApp.view2'));
 
+  var nameServiceMock;
+  var scopeMock;
+
+  beforeEach(module(function($provide) {
+    nameServiceMock = {
+      getName: function() {},
+      setName: function(name) {}      
+    };
+
+    scopeMock = {
+      $watch: function() {}
+    }
+
+    $provide.value('nameService', nameServiceMock);
+    $provide.value('$scope', scopeMock);
+    
+  }));
+
   describe('view2 controller', function(){
 
     it('should ....', inject(function($controller) {
