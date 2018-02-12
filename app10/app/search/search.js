07b463483a7d7df4011f9cc47a9f9c03d403a7a3
farooqam <farooq_am@hotmail.com>
2018-02-12 10:21:26 -0800

    app10 - ng-repeat, hash name and address

--- app10/app/search/search.js
+++ app10/app/search/search.js
@@ -2,43 +2,54 @@
 
 angular.module('myApp.search', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/search', {
-    templateUrl: 'search/search.html',
-    controller: 'SearchCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/search', {
+            templateUrl: 'search/search.html',
+            controller: 'SearchCtrl'
+        });
+    }])
 
-.controller('SearchCtrl', ['$log', function($log) {
-  var self = this;
-  
-  self.person = {
-      name: 'Bubba Gump',
-      address: '100 1st Ave, New York, NY 10010'
-  };
+    .controller('SearchCtrl', ['$log', function ($log) {
+        var self = this;
 
-  self.metadata = {
-      id: 100,
-      description: 'foo'
-  };
+        self.people = [
+            {
+                name: 'Bubba Gump',
+                address: '100 1st Ave, New York, NY 10010'
+            },
+            {
+                name: 'Farooq Mahmud',
+                address: '100 1st Ave, New York, NY 10010'
+            },
+            {
+                name: 'Emily Green',
+                address: '100 1st Ave, New York, NY 10010'
+            }
+        ];
 
-  self.hashMetadata = function(metadata) {
-    let stringToHash = `${metadata.id}${metadata.description.toLowerCase()}`;
-    let hash = sha256(stringToHash);
-    $log.info(JSON.stringify({str: stringToHash, hash: hash}));
-    return hash;
-  };
-
-}])
-.directive("searchResult", ['$log', function($log) {
-    return {
-        templateUrl: 'search/searchResult.html',
-        replace: true,
-        scope: {
-            header: '@',
-            text: '@',    
-            metadata: '=',
-            hashFn: '&'        
+        self.metadata = {
+            id: 100,
+            description: 'foo'
+        };
+        
+        self.hashPerson = function(person) {
+            let stringToHash = `${person.name.toLowerCase()}${person.address.toLowerCase()}`;
+            let hash = sha256(stringToHash);
+            $log.info(JSON.stringify({ str: stringToHash, hash: hash }));
+            return hash;
         }
-    };
-}]);
+
+    }])
+    .directive("searchResult", ['$log', function ($log) {
+        return {
+            templateUrl: 'search/searchResult.html',
+            replace: true,
+            scope: {
+                header: '@',
+                text: '@',
+                metadata: '=',
+                hashFn: '&',
+                person: '='
+            }
+        };
+    }]);
