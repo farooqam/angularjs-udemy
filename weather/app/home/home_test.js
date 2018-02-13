a6f4dacd2080b6c21fa07a77639419167bcd8c3a
farooqam <farooq_am@hotmail.com>
2018-02-13 08:06:21 -0800

    weather app - home view

---
+++ weather/app/home/home_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('weatherApp.home module', function() {
+
+  beforeEach(module('weatherApp.home'));
+
+  describe('home controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var homeController = $controller('HomeController');
+      expect(homeController).toBeDefined();
+    }));
+
+  });
+});
